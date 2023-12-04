68e518733e32cb3356e120654229a50bdb5dfb8c
Paulo Monteiro <pamte@vestas.com>
2023-12-04 13:34:02 +0000

    changes 4/12 proj e digital ocean

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
+use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+use secrecy::ExposeSecret;
+
+
+
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    
+    
+    let subscriber = get_subscriber("zero2prod".into(), 
+    "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
+
+    let configuration = get_configuration()
+    .expect("Failed to read configuration.");
+
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(
+            configuration.database.connection_string().expose_secret()
+        )
+        .expect("Failed to connect to Postgress connection pool.");
+    
+    // We have removed the hard-coded '8000' - it's now coming from our settings!
+    let address = format!("{}:{}", 
+    configuration.application.host, configuration.application.port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,70 @@
+use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        //request_id = %Uuid::new_v4(),
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
+
+// Let's start simple: we always return a 200 OK
+pub async fn subscribe(
+    form: web::Form<FormData>, 
+    pool: web::Data<PgPool>) -> HttpResponse {
+
+    match insert_subscriber (&pool, &form).await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) =>  HttpResponse::InternalServerError().finish()
+    }
+}
+    #[tracing::instrument(
+        name = "Saving new subscriber details in the database",
+        skip(form, pool)
+    )]
+    
+    pub async fn insert_subscriber (
+        pool: &PgPool,
+        form: &FormData,
+    ) -> Result<(), sqlx::Error> {
+
+        sqlx::query!(
+            r#"
+                     INSERT INTO subscriptions (id, email, name, subscribed_at) 
+                     VALUES ($1, $2, $3, $4)
+                     "#,
+            Uuid::new_v4(),
+            form.email,
+            form.name,
+            Utc::now()
+        )
+        // We use `get_ref` to get an immutable reference to the `PgConnection`
+        // wrapped by `web::Data`.
+        .execute(pool)
+        .await
+        .map_err(|e | {
+            tracing::error!("Failed to execute query: {:?}", e);
+            e
+            // Using the `?` operator to return early
+            // if the function failed, returning a sqlx::Error
+            // We will talk about error handling in depth later!
+        })?;
+Ok(())
+    }
+
+
+   
+  
+
